739ba6f410f93c779de9b8f7cbd05407dd0e9f65
GitOfDirector <1191549194@qq.com>
2018-11-26 18:22:51 +0800

    排列组合问题

---
+++ DataStructureAndAlgorithm/PermutationAndCombination/PCMethods.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PermutationAndCombination
+{
+    class PCMethods<T>
+    {
+        //参考： https://blog.csdn.net/cxdragoon/article/details/70229772
+        //           https://www.cnblogs.com/kissdodog/p/5419981.html
+
+        /// <summary>
+        /// 求数组中n个元素的排列
+        /// </summary>
+        /// <param name="source">所求数组</param>
+        /// <param name="n">元素个数</param>
+        /// <returns>数组中n个元素的排列</returns>
+        public static List<T[]> GetPermutation(T[] source, int n)
+        {
+            if (n > source.Length)
+                return null;
+
+            List<T[]> list = new List<T[]>();
+
+            //首先得到n个不同元素的组合
+            List<T[]> c= GetCombinationList2(source, n);
+            
+            for (int i = 0; i < c.Count; i++)
+            {
+                List<T[]> tl = new List<T[]>();
+                
+                //得到一个组合的所有排列
+                GetPermutationList(ref tl, c[i], 0, n - 1);
+                
+                list.AddRange(tl);
+            }
+
+            return list;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="list"></param>
+        /// <param name="eles">需要排列的元素</param>
+        /// <param name="startIndex">开始下标</param>
+        /// <param name="endIndex">结束下标</param>
+        private static void GetPermutationList(ref List<T[]> list, T[] eles, int startIndex, int endIndex)
+        {
+            if (startIndex == endIndex)
+            {
+                if (list == null)
+                {
+                    list = new List<T[]>();
+                }
+
+                T[] temp = new T[eles.Length];
+                eles.CopyTo(temp, 0);
+                list.Add(temp);
+            }
+            else
+            {
+                for (int i = startIndex; i <= endIndex; i++)
+                {
+                    //交换 起始位置 每一个位置 的值---一个新的排列
+                    Swap(ref eles[startIndex], ref eles[i]);
+                    
+                    //进行下一个位置
+                    GetPermutationList(ref list, eles, startIndex + 1, endIndex);
+                    
+                    //对于每一个新的排列，再次交换新的 下一个起始位置 每一个位置
+                    Swap(ref eles[startIndex], ref eles[i]);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 交换两个变量
+        /// </summary>
+        /// <param name="a">变量1</param>
+        /// <param name="b">变量2</param>
+        public static void Swap(ref T a, ref T b)
+        {
+            T temp = a;
+            a = b;
+            b = temp;
+        }
+
+        /// <summary>
+        /// 从n个不同元素中任意选取m个元素的组合的所有组合
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="elements"></param>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static List<List<T>> GetCombinationList(List<T> elements, int n)
+        {
+            List<List<T>> result = new List<List<T>>();
+
+            int eleNum = elements.Count;
+            
+            //将传递进来的元素列表拷贝出来进行处理，防止后续步骤修改原始列表，造成递归返回后原始列表被修改；
+            List<T> source = new List<T>(elements);
+
+            //当只选取一个元素的时候，就是将集合中的每一个输出
+            if (n == 1)
+            {
+                foreach (var item in source)
+                {
+                    List<T> itemGroup = new List<T>();
+                    itemGroup.Add(item);
+                    result.Add(itemGroup);
+                }
+
+                return result;
+            }
+           
+            for (int i = 0; i < eleNum; i++)
+            {
+                //否则，等于选取 当前元素 + 选取n - 1个元素的组合
+                T curEle = source[0];
+                source.RemoveAt(0);
+                List<List<T>> tempRes = GetCombinationList(source, n - 1);
+                
+                //给每一个当前组合添加上当前元素
+                for (int j = 0; j < tempRes.Count; j++)
+                {
+                    List<T> itemGroup = new List<T>();
+                    itemGroup.Add(curEle);
+                    itemGroup.AddRange(tempRes[j]);
+
+                    result.Add(itemGroup);
+                }
+            }
+
+            return result;
+        }
+
+        public static List<T[]> GetCombinationList2(T[] arr, int n)
+        {
+            if (arr.Length < n)
+                return null;
+
+            List<T[]> list = new List<T[]>();
+            int[] selectIndex = new int[n];
+            GetCombination(ref list, arr, arr.Length, n, selectIndex, n);
+            
+            return list;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="list">结果</param>
+        /// <param name="eles">选取的元素集合</param>
+        /// <param name="elesCount">选取元素的个数</param>
+        /// <param name="needSelectCount">当前剩余选取的元素个数</param>
+        /// <param name="indexes">下标数组</param>
+        /// <param name="selectCount">选取的元素个数</param>
+        private static void GetCombination(ref List<T[]> list, T[] eles, int elesCount, int needSelectCount, int[] indexes, int selectCount)
+        {
+            //问题转化为从0 --- eleCount - 1 的下标中选取 指定数量的下标
+
+            //在元素个数大于需要数量时，缩小待选集合
+            for (int i = elesCount; i >= needSelectCount; i--)
+            {
+                //当剩余选取元素个数大于1个时，接着选取
+                //否则，表示一次选取完成
+                indexes[needSelectCount - 1] = i - 1;
+
+                //当剩余选取元素个数大于1个时，接着选取
+                if (needSelectCount > 1)
+                {
+                    GetCombination(ref list, eles, i - 1, needSelectCount - 1, indexes, selectCount);
+                }
+                else
+                {
+                    if (list == null)
+                    {
+                        list = new List<T[]>();
+                    }
+
+                    T[] temp = new T[selectCount];
+                    for (int j = 0; j < indexes.Length; j++)
+                    {
+                        //Console.Write(indexes[j] + "   ");
+                        int curIndex = indexes[j];
+                        temp[j] = eles[curIndex];
+                    }
+                    //Console.WriteLine();
+
+                    list.Add(temp);
+                }
+            }
+        }
+
+    }
+}
---
+++ DataStructureAndAlgorithm/PermutationAndCombination/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PermutationAndCombination
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<string> res = LetterCasePermutation_BFS("a1b2");
+            //List<string> res = LetterCasePermutation_DFS("a1b2");
+
+            //foreach (var item in res)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //List<int> eles = new List<int>() { 1, 2, 3 };
+            //List<List<int>> combinations = PCMethods<int>.GetCombinationList(eles, 2);           
+
+            //int[] arr = { 1, 2, 3, 4, 5 };
+            //List<int[]> combinations = PCMethods<int>.GetCombinationList2(arr, 3);
+
+            int[] arr = { 1, 2, 3, 4 };
+            List<int[]> res = PCMethods<int>.GetPermutation(arr, 4);
+            ConsoleReult(res);
+
+            Console.ReadKey();
+
+        }
+
+
+        public static void ConsoleReult(List<int[]> result) 
+        {
+            for (int i = 0; i < result.Count; i++)
+            {
+                //Console.Write($"{i}:  ");
+                Console.Write("{0, 4} :  ", i);
+                for (int j = 0; j < result[i].Length; j++)
+                {
+                    Console.Write(result[i][j] + "  ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        /*
+         * 有一字符串，我们可以将其中的字母转换成大写或者小写，则变换后可以拥有多少字符串？
+         * 例如：a1b2 ===》a1b2, A1b2, a1B2, A1B2
+         * 
+         * 我们可以看做一个拥有虚拟根的树，左侧是小写字母，右侧是大写字母
+         *                          a                                           A
+         *             ab                  aB                   Ab                  AB
+         *      abc     abC     aBc     aBC      Abc       AbC    ABc      ABC 
+         *      
+         * 
+         * 该问题也属于abc下标组合问题
+         * 即三个不同下标选出任意个下标转换成大写的组合方式有多少种
+         *          3选0 ：abc
+         *          3选1 ：Abc,   aBc,    abC --- {0, 1, 2}下标选1个
+         *          3选2 ：ABc,   AbC,   aBC --- {0, 1, 2}下标选2个
+         *          3选3 ：ABC -- {0, 1, 2}下标选3个
+         */
+
+        public static List<string> LetterCasePermutation_BFS(String S)
+        {
+            if (string.IsNullOrEmpty(S))
+            {
+                return new List<string>();
+            }
+
+            Queue<string> que = new Queue<string>();
+            que.Enqueue(S);
+
+            for (int i = 0; i < S.Length; i++)
+            {
+                if (char.IsDigit(S[i]))
+                    continue;
+
+                int size = que.Count;
+                for (int j = 0; j < size; j++)
+                {
+                    string cur = que.Dequeue();
+                    char[] chars = cur.ToCharArray();
+
+                    chars[i] = char.ToUpper(chars[i]);
+                    que.Enqueue(new string(chars));
+
+                    chars[i] = char.ToLower(chars[i]);
+                    que.Enqueue(new string(chars));
+                }
+            }
+
+            return new List<string>(que);
+        }
+
+        public static List<string> LetterCasePermutation_DFS(String S)
+        {
+            if (string.IsNullOrEmpty(S))
+            {
+                return new List<string>();
+            }
+
+            List<string> res = new List<string>();
+            DfsString(S.ToCharArray(), 0, res);
+
+            return res;
+        }
+
+        public static void DfsString(char[] chars, int index, List<string> res)
+        {
+            if (index == chars.Length)
+            {
+                res.Add(new string(chars));
+                return;
+            }
+
+            if (char.IsDigit(chars[index]))
+            {
+                DfsString(chars, index + 1, res);
+            }
+            else
+            {
+                chars[index] = char.ToUpper(chars[index]);
+                DfsString(chars, index + 1, res);
+
+                chars[index] = char.ToLower(chars[index]);
+                DfsString(chars, index + 1, res);
+            }
+
+        }
+
+    }
+}
